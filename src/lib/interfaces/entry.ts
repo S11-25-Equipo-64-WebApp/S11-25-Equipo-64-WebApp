50929d6a23c1b5c93972097bec9a764c7e101aa3
Davis <64158093+Davis-3450@users.noreply.github.com>
2025-12-08 06:57:09 -0500

    feat(entry): add EntryStatus enum and update EntryData interface for status management

--- src/lib/interfaces/entry.ts
+++ src/lib/interfaces/entry.ts
@@ -1,8 +1,16 @@
+import { MediaSource } from "../constants/media-sources";
+import { EntryStatus } from "../enums/entry-status";
+
 export interface EntryData { 
     id: string // UUID v4
     title: string;
-    content: string;
+    content?: string;
     mediaUrl?: string;
-    createdAt: Date;
+    mediaSource: MediaSource;
+    summary?: string;
+
+    date: Date; // anything for simplicity
     tags?: string[];
+    author: string;
+    status: EntryStatus;
 }
